282a532146d90cd1308ba8422ffbc5c7bd022d14
Trevor Nelson <trevorjohnnels@gmail.com>
2019-11-03 15:01:05 -0700

    3.2.13.2.13.2.1

--- 320instanceVars.rb
+++ 320instanceVars.rb
@@ -1,24 +1,30 @@
 class FakeClass
-    def class_method
-      puts "1 1 1 1 1 1 1 1 1 1 1  1  1 1 1"
-        nelson = "ice cream is life"
-        @trevor = "trevor loves icecream"
-    end
-     def class_method_two
-       puts "2 2 2 2 2 2 2 2 2 2 2  2  2 2 2"
-       puts @trevor  # => “Trevor”
-       puts nelson # => “no_method_error!!!!!!!!”
-    end
-    def class_method_three
-      puts "3 3 3 3 3 3 3 3 3 3 3  3  3 3 3"
-      puts nelson # => “no_method_error!!!!!!!!”
-   end
+  def initialize
+    puts "_-_-_-_-_-_-_-_-_-_-_-_-"
+    @trevor = "trevor loves icecream"
+    puts "making a new FakeClass instance"
+    puts "_-_-_-_-_-_-_-_-_-_-_-_-"
+  end
+  def class_method
+    puts "1 1 1 1 1 1 1 1 1 1 1  1  1 1 1"
+    @trevor = "trevor loves icecream"
+    nelson = "ice cream is life"
+  end
+  def class_method_two
+   puts "2 2 2 2 2 2 2 2 2 2 2  2  2 2 2"
+   puts @trevor  # => “Trevor”
+   puts nelson # => “no_method_error!!!!!!!!”
+  end
+  def class_method_three
+    puts "3 3 3 3 3 3 3 3 3 3 3  3  3 3 3"
+    puts nelson # => “no_method_error!!!!!!!!”
+ end
 end
 
 
 athing = FakeClass.new
 
-athing.class_method
+# athing.class_method
 athing.class_method_two
 puts "$ $ $ $ $ $ $ $ $ $ $$  $$  $ $ $"
 athing.class_method_three
